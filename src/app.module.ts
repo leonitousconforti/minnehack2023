886b77e66eeb845bde13843236f05bc97b93a666
aaaaaaaaa <mill9134@umn.edu>
2023-01-28 20:51:35 -0600

    ???

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from "@nestjs/common";
 import { IndexModule } from "./pages/index/index.module";
 import { LoginModule } from "./pages/login/login.module";
 import { RegisterModule } from "./pages/register/register.module";
+import { SubmissionModule } from "./pages/submission/submission.module";
 
-@Module({ imports: [IndexModule, LoginModule, RegisterModule] })
+@Module({ imports: [IndexModule, LoginModule, RegisterModule, SubmissionModule] })
 export class AppModule {}
