f73f1c8b893045ecd2f6730d362a3a1213c04fdf
aaaaaaaaa <mill9134@umn.edu>
2023-01-28 22:58:53 -0600

    register stuff

--- src/pages/register/register.controller.ts
+++ src/pages/register/register.controller.ts
@@ -1,10 +1,30 @@
-import { Controller, Get, Render } from "@nestjs/common";
+import { Body, Controller, Get, Post, Render, Res } from "@nestjs/common";
+import { Response } from "express";
+import { UsersService } from "src/users/users.service";
 
 @Controller()
 export class RegisterController {
+    constructor(private usersService: UsersService) {}
     @Get("/register")
     @Render("register")
-    root() {
-        return { message: "Hello world!" };
+    root(thing) {
+        return {message: thing};
+    }
+    @Post("/register")
+    async post(@Body() details, @Res() response: Response) {
+        console.log(details)
+        if(details.psw != details.psw2){
+            response.render("register", {message: "Passwords do not match."});
+        }
+        else if(await this.usersService.findOne(details.uname) != undefined) {
+            response.render("register", {message: "User already exists."});
+        }
+        else {
+            this.usersService.create({
+                username: details.uname,
+                password: details.psw
+            });
+            response.render("login");
+        }
     }
 }
