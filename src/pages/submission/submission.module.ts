1747ef8c7306413b9f0cde8642ea77a8720d9afa
jklpo360 <44623646+jklpo360@users.noreply.github.com>
2023-01-29 00:03:03 -0600

    Delete src directory

--- src/pages/submission/submission.module.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from "@nestjs/common";
-
-import { SubmissionController } from "./submission.controller";
-
-@Module({ controllers: [SubmissionController] })
-export class SubmissionModule {}
